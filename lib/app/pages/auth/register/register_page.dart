aa5c43b728abb82468acd6ce24113c61abd0a996
jacksonAbrao <jackson.comp1681@hotmail.com>
2023-02-06 01:12:44 -0300

    terminado a terceira aula

---
+++ lib/app/pages/auth/register/register_page.dart
@@ -0,0 +1,82 @@
+import 'package:delivery_app/app/core/ui/styles/text_styles.dart';
+import 'package:delivery_app/app/core/ui/widgets/delivery_app_bar.dart';
+import 'package:delivery_app/app/core/ui/widgets/delivery_button.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DeliveryAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Cadastro',
+                  style: context.textStyles.textTitle,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Preencha os campos abaixo para criar seu cadastro.',
+                  style: context.textStyles.textMedium.copyWith(
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Nome',
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'E-mail',
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Senha',
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Confirmar Senha',
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Center(
+                  child: DeliveryButton(
+                    label: 'Cadastrar',
+                    onPressed: () {},
+                    width: double.infinity,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
